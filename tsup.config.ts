da316eace9e901163e133cbdb0e0e2d72b5caee5
that.webdev.dude <that.webdev.dude@gmail.com>
2026-03-12 07:58:57 +0000

    chore: add MIT license, update package.json and tsconfig and refine test configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entry: ["src/index.ts"],
-    format: ["esm", "cjs"],
-    dts: true,
-    sourcemap: true,
-    clean: true,
-    target: "es2022",
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  platform: "browser",
+  target: "es2020",
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: "node",
-    include: ["tests/unit/**/*.test.ts", "tests/integration/**/*.test.ts"],
+    include: [
+      "src/**/*.test.ts",
+      "tests/unit/**/*.test.ts",
+      "tests/integration/**/*.test.ts",
+    ],
   },
 });
